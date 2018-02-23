4b0b6f63d9755988530f554697a1a5e04fe89806
streetart <overloadedpixel@gmail.com>
2018-02-23 19:44:08 +0200

    page nav

--- Spotr-app/src/app/app.module.ts
+++ Spotr-app/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    UserComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '',
+        component: HomeComponent
+      },
+      { path: 'component',
+        component: ComponentComponent
+      },
+      { path: 'user',
+        component: UserComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
